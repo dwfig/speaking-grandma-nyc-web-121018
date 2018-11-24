d693c70953fe79c86572f8c92acb96113cb250ae
Daniel Fries <daniel.w.fries@gmail.com>
2018-11-24 21:10:38 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -6,7 +6,7 @@
 def speak_to_grandma(phrase)
   if phrase == phrase.upcase
     "NO, NOT SINCE 1938!"
-  elsif phrase !== phrase.upcase
+  elsif !(phrase == phrase.upcase)
     "HUH?! SPEAK UP, SONNY!"
   elsif phrase == "I LOVE YOU GRANDMA!"
     "I LOVE YOU TOO PUMPKIN!"
