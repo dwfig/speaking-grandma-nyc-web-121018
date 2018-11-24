136b3e1d2487e21d49653d95f134298f16c94394
Daniel Fries <daniel.w.fries@gmail.com>
2018-11-24 21:10:20 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -4,10 +4,10 @@
 # HUH?! SPEAK UP, SONNY!
 # unless you shout it (type in all capitals).
 def speak_to_grandma(phrase)
-  if phrase !== phrase.upcase
-    "HUH?! SPEAK UP, SONNY!"
-  elsif phrase == phrase.upcase
+  if phrase == phrase.upcase
     "NO, NOT SINCE 1938!"
+  elsif phrase !== phrase.upcase
+    "HUH?! SPEAK UP, SONNY!"
   elsif phrase == "I LOVE YOU GRANDMA!"
     "I LOVE YOU TOO PUMPKIN!"
   end
